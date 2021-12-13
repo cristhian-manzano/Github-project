0e2dd6ac2ece35d4d69b2df1d0f11b818ec5fe0f
Cristhian Manzano <crisven1998@gmail.com>
2021-12-13 13:24:21 -0500

    Adding context GithubContext to App

--- src/components/users/UserItem.jsx
+++ src/components/users/UserItem.jsx
@@ -18,8 +18,9 @@ function UserItem({ user: { login, avatar_url } }) {
           <Link
             className="text-base-content text-opacity-40"
             to={`/users/${login}`}
-          />
-          Visit profile
+          >
+            Visit profile
+          </Link>
         </div>
       </div>
     </div>
